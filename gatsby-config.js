195badc948018f22e8fdb1a21941b42a7a61773b
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2019-02-22 12:12:09 -0400

    Wired graphql to markdown blogs

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: `Pands Eating Lots`,
+    title: `Pandas Eating Lots`,
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
   {
     resolve: `gatsby-plugin-typography`,
