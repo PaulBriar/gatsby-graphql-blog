96fbf90542462bedf5a1a648b20f09778d2f7ec5
Paul Briar <35573577+PaulBriar@users.noreply.github.com>
2019-02-27 11:26:51 -0400

    Completed v1, added PWA & Offline plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,22 @@ module.exports = {
     },
     `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
+    {
+          resolve: `gatsby-plugin-manifest`,
+          options: {
+            name: "GatsbyJS",
+            short_name: "GatsbyJS",
+            start_url: "/",
+            background_color: "#6b37bf",
+            theme_color: "#6b37bf",
+            // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+            // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+            display: "standalone",
+            icon: "src/images/favicon.ico", // This path is relative to the root of the site.
+          },
+        },
+          `gatsby-plugin-offline`,
+          `gatsby-plugin-react-helmet`,
   {
     resolve: `gatsby-plugin-typography`,
     options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,64 @@
 import React from "react";
-import { graphql } from 'gatsby';
+import { Link, graphql } from 'gatsby';
+import { Helmet } from 'react-helmet';
 import { css } from '@emotion/core';
 import { rhythm } from '../utils/typography';
 import Layout from '../components/layout';
 
 export default ({ data }) => {
-  console.log(data);
   return (
-    <Layout>
-      <div>
-        <h1
-          css={css`
-            display: inline-block;
-            border-bottom: 1px solid;
-          `}
-        >Amazing Pandas Eating Things</h1>
-        <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-        {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
-            <h3
-              css={css `
-                margin-bottom: ${rhythm(1 / 4)};
-              `}
-              >
-                {node.frontmatter.title}{" "}
-                <span
-                  css={css`
-                    color: #bbb;
-                  `}
+    <div className="application">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Amazing Pandas Eating Things</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+      </Helmet>
+      <Layout>
+        <div>
+          <h1
+            css={css`
+              display: inline-block;
+              border-bottom: 1px solid;
+            `}
+          >Amazing Pandas Eating Things</h1>
+          <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+          {data.allMarkdownRemark.edges.map(({ node }) => (
+            <div key={node.id}>
+              <Link
+                to={node.fields.slug}
+                css={css`
+                  text-decoration: none;
+                  color: inherit;
+                `
+                }
                 >
-                  - {node.frontmatter.date}
-                </span>
-              </h3>
-              <p>{node.excerpt}</p>
-              <hr/>
-          </div>
-        ))}
-      </div>
-    </Layout>
+                <h3
+                  css={css `
+                    margin-bottom: ${rhythm(1 / 4)};
+                  `}
+                  >
+                    {node.frontmatter.title}{" "}
+                    <span
+                      css={css`
+                        color: #bbb;
+                      `}
+                    >
+                      - {node.frontmatter.date}
+                    </span>
+                  </h3>
+                  <p>{node.excerpt}</p>
+                  <hr/>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </Layout>
+    </div>
 )};
 
 export const query = graphql`
         query {
-          allMarkdownRemark {
+          allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
             totalCount
             edges {
               node {
@@ -50,6 +66,9 @@ export const query = graphql`
                 frontmatter {
                   title
                   date(formatString: "DD MMMM, YYYY")
+                }
+                fields {
+                  slug
                 }
                 excerpt
               }
